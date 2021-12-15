b00d172f88bb476dd50a13eb74d18d05e19c5bbd
gauravbasantani <90026794+gauravbasantani@users.noreply.github.com>
2021-12-15 12:50:04 +0530

    cm

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import StudInfo from './StudInfo';
+
+function App() {
+  return (
+    <>
+    <StudInfo />
+    
+    </>
+  );
+}
+
+export default App;
